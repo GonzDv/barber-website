30e01ce0e185ca5732a8a5a544f2df60cc37a892
Julian Gnz <63946576+GonzDv@users.noreply.github.com>
2024-08-15 23:35:47 +0000

    feat: new sections and a image added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['"Manrope"', 'sans-serif'],
+    }
+    
   },
   plugins: [],
 }
