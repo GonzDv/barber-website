0eb5c499017815cc67eef91859ae97dcc6071d59
Julian Gnz <63946576+GonzDv@users.noreply.github.com>
2024-08-15 16:00:49 +0000

    fix: footer at mobile screen

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -3,9 +3,9 @@ import BookForm from "../components/Recervar-cita/BookForm.jsx";
 export default function Book() {
   return (
     <>
-      <main className="h-svh container mx-auto">
+      <main className="lg:h-screen  container mx-auto mt-6">
         <div className="container mx-auto text-center">
-        <h2 className=" font-bold uppercase py-5">reservar cita</h2>
+        <h2 className=" font-bold uppercase py-6 ">reservar cita</h2>
         </div>
         <BookForm />
       </main>
